89dd7a2ba7ea1abfead86bf5550b323580b8b378
Richard Rhodes <rafrhodes@yahoo.co.uk>
2020-12-17 16:18:54 +0000

    Update three_roaming_scraper.py
    find the search box, input the destination and navigate to that page, then print page title to verify country

--- three_roaming_scraper.py
+++ three_roaming_scraper.py
@@ -10,3 +10,14 @@ driver = webdriver.Chrome(PATH)
 
 driver.get("http://www.three.co.uk/Support/Roaming_and_international/Roaming_abroad")
 print (driver.title)
+
+search = driver.find_element_by_id("input-append-demo")
+search.clear()
+search.send_keys("Brazil")
+search.send_keys(Keys.RETURN)
+
+# Driver Tite should give title of current country page - can pull this off later to head
+# up details of scraped info
+print(driver.title)
+
+#driver.quit()
