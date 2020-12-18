882a3abd4b65f07cbbd3d1cae07778039e25184c
Richard Rhodes <rafrhodes@yahoo.co.uk>
2020-12-18 00:11:09 +0000

    Looping through countries and now displaying their names in the same way on the console. Next to pull data from the relevant tables.

--- three_roaming_scraper.py
+++ three_roaming_scraper.py
@@ -11,6 +11,7 @@ driver = webdriver.Chrome(PATH)
 driver.get("http://www.three.co.uk/Support/Roaming_and_international/Roaming_abroad")
 print (driver.title)
 
+
 countries = ["Brazil", "South Africa", "Portugal", "Chile", "Iceland", "China", "Madagascar"]
 
 # Loop through countries
@@ -21,13 +22,27 @@ for country in countries:
 	search.send_keys(country)
 	search.send_keys(Keys.RETURN)
 
-	# Driver Tite should give title of current country page - can pull this off later to head
+	try: 
+		nation = driver.find_element_by_xpath("//*[@class='flag-links']/img[@alt]")
+		print("Mobile roaming in " + nation.get_attribute("alt") + " - Support - Three")
+
+	except:
+
+		print(driver.title)
+
+
+
+	three_col_table = driver.find_element_by_class_name("clearfix")
+
+	# Driver Title should give title of current country page - can pull this off later to head
 	# up details of scraped info
 
 	# *** UPDATE *** Different pages present different tables. 2 column tables don't display the
 	# country in "driver.title" but DO show country in flag "Alt-text". need to setablish 
 	# what table is on the page and then either print driver.title OR flag-links alt-text
-	print(driver.title)
+
+	#if two_col_table:
+
 
 	# go back to roaming abroad home page via initial link as bredcrumbs not consistent on older pages
 	driver.get("http://www.three.co.uk/Support/Roaming_and_international/Roaming_abroad")
